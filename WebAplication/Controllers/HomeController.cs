06dd8b53c2576fc8b720157e94b3d3b2a82ffd51
Michał <miszalaty@gmail.com>
2023-02-12 17:52:25 +0100

    Add project files.

---
+++ WebAplication/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAplication.Models;
+
+namespace WebAplication.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [Route("testowy-Route/{name}")]
+        public IActionResult Produkt(string name)
+        {
+            return View();
+        }
+    }
+}
---
+++ WebAplication/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAplication.Models;
+
+namespace WebAplication.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Product()
+        {
+            var product = new Product
+            {
+                Id = 1,
+                Category = "samochód",
+                Description = "To jest super auto",
+                Name = "BMW"
+            };
+            return View(product);
+        }
+        public IActionResult List()
+        {
+            var productList = new List<Product>
+            {
+                new Product
+                {
+                    Id = 2,
+                Category = "samochód",
+                Description = "To jest super auto",
+                Name = "KIA"
+                },
+                 new Product
+                {
+                    Id = 3,
+                Category = "samochód",
+                Description = "To jest super auto",
+                Name = "honda"
+                }
+            };
+            return View(productList);
+        }
+        public IActionResult Data()
+        {
+            return View();
+        }
+    }
+}
